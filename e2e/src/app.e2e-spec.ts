5bb18c2b9d616661ff970ffa94c87e596f844337
Migiway <marc.iguenane@gmail.com>
2019-03-07 20:11:44 +0100

    stash

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to blyatProject!');
+    expect(page.getParagraphText()).toEqual('Welcome to blyat!');
   });
 });
